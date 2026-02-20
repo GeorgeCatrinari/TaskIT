a9fbea536f98b6ae749732ca00e9d54032f4ee5e
GeorgeCatrinari <georgecatrinari@gmail.com>
2026-02-20 18:24:11 +0200

    Tasks Comp created, show the name of selected user

--- src/app/user/user.ts
+++ src/app/user/user.ts
@@ -1,4 +1,4 @@
-import { Component,Input, Output, EventEmitter, output } from '@angular/core';
+import { Component,Input, Output, EventEmitter} from '@angular/core';
 
 @Component({
   selector: 'app-user',
@@ -10,11 +10,7 @@ export class User {
   @Input({required: true}) id!: string;
   @Input({required: true}) avatar!: string;
   @Input({required: true}) name!: string;
-  //@Output() select = new EventEmitter();
-  //id = input...
-  //avatar = input..
-  //name = input..
-  select = output<string>(); //same code as @Output, doesnt create a signal, its still an event
+  @Output() select = new EventEmitter();
 
   get imagePath() {
     return 'assets/users/' + this.avatar;
