8a6a9944474df13adbc92b889e3f0c344da91eb3
GeorgeCatrinari <georgecatrinari@gmail.com>
2026-02-20 12:50:11 +0200

    Changed the State Management for User from Zone.js to Signals

--- src/app/user/user.ts
+++ src/app/user/user.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 
 import { DUMMY_USERS } from '../dummy-users';
 
@@ -11,13 +11,14 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
   styleUrl: './user.css',
 })
 export class User {
-  selectedUser = DUMMY_USERS[randomIndex];
+  selectedUser = signal(DUMMY_USERS[randomIndex]);
+  imagePath = computed(() => 'assets/users/' + this.selectedUser().avatar);
 
-  get imagePath(){
-    return 'assets/users/' + this.selectedUser.avatar;
-  }
+  // get imagePath(){
+  //   return 'assets/users/' + this.selectedUser.avatar;
+  // }
 
   onSelectUser(){
-    this.selectedUser = DUMMY_USERS[Math.floor(Math.random() * DUMMY_USERS.length)];
+    this.selectedUser.set(DUMMY_USERS[Math.floor(Math.random() * DUMMY_USERS.length)]);
   }
 }
