80a61dc86376187e3bd463ffa089dce9e6333e84
GeorgeCatrinari <georgecatrinari@gmail.com>
2026-02-25 11:47:04 +0200

    Outputting Task Data in the Task Component, created User and Task interfaces

--- src/app/user/user.ts
+++ src/app/user/user.ts
@@ -1,5 +1,11 @@
 import { Component,Input, Output, EventEmitter} from '@angular/core';
 
+interface UserItem {
+    id : string;
+    name : string;
+    avatar: string;
+};
+
 @Component({
   selector: 'app-user',
   imports: [],
@@ -7,11 +13,7 @@ import { Component,Input, Output, EventEmitter} from '@angular/core';
   styleUrl: './user.css',
 })
 export class User {
-  @Input({required : true}) user!: {
-    id : string;
-    name : string;
-    avatar: string;
-  };
+  @Input({required : true}) user!: UserItem;
   @Output() select = new EventEmitter();
 
   get imagePath() {
